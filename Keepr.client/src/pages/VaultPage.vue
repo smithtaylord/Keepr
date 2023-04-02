dabf3addfcb9df7b45f4ee755f4b7f3e5b26fd9f
Taylor Smith <smith.taylord@gmail.com>
2023-04-02 09:21:03 -0600

    keep data draws

--- Keepr.client/src/pages/VaultPage.vue
+++ Keepr.client/src/pages/VaultPage.vue
@@ -1,7 +1,38 @@
 <template>
-    <div class="component">
-        THIS IS THE VAULT PAGE YO!
+    <div v-if="vault" class="container-fluid">
+        <div class="row">
+            <div class="col-4 m-auto">
+                <div class="position-relative">
+                    <div>
+                        <img class="vault-pic img-fluid rounded mt-4" :src="vault.img" :alt="vault.name">
+                    </div>
+                    <div class="position-absolute bottom-0 w-100">
+                        <div class="text-center pb-3">
+                            <h1 class="text-light text-shadow font-q">{{ vault.name }}</h1>
+                            <p class="text-light text-shadow font-q fs-5">by {{ vault.creator.name }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-end px-2">
+                    <i class="mdi mdi-dots-horizontal text-dark selectable fs-3" title="edit or delete vault"></i>
+                </div>
+                <div class="row">
+                    <div class="col-4 m-auto">
+                        <div class="text-center">
+                            <p v-if="vaultKeeps.length == 1" class="keeps-count bg-secondary font-o fs-4 fw-bold p-1">{{
+                                vaultKeeps.length }} Keep</p>
+                            <p v-else class="keeps-count bg-secondary font-o fs-4 fw-bold p-1">{{ vaultKeeps.length }} Keeps
+                            </p>
 
+                        </div>
+                    </div>
+                </div>
+                {{ vault }}
+            </div>
+        </div>
+    </div>
+    <div>
+        {{ vaultKeeps }}
     </div>
 </template>
 
@@ -11,7 +42,8 @@ import { useRoute, useRouter } from 'vue-router';
 import Pop from '../utils/Pop.js';
 import { vaultsService } from '../services/VaultsService.js';
 import { vaultKeepsService } from '../services/VaultKeepsService.js';
-import { watchEffect } from 'vue';
+import { watchEffect, computed } from 'vue';
+import { AppState } from '../AppState.js';
 
 export default {
     setup() {
@@ -39,10 +71,23 @@ export default {
                 getVaultKeeps()
             }
         })
-        return {}
+        return {
+            vault: computed(() => AppState.vault),
+            vaultKeeps: computed(() => AppState.vaultKeeps)
+        }
     }
 }
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.vault-pic {
+    height: 250px;
+    width: 100%;
+    object-fit: cover;
+}
+
+.keeps-count {
+    border-radius: 15px;
+}
+</style>
